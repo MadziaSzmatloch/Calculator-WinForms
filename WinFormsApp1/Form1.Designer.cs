2b9edd296f176d2fc78d08fd7760c658ab8d6dbc
MadziaSzmatloch <98941475+MadziaSzmatloch@users.noreply.github.com>
2023-03-16 19:28:13 +0100

    Many errors are safe now
    errors fixed
    cute style added

--- WinFormsApp1/Form1.Designer.cs
+++ WinFormsApp1/Form1.Designer.cs
@@ -52,122 +52,146 @@
             // 
             // button7
             // 
+            button7.BackColor = Color.Thistle;
+            button7.FlatStyle = FlatStyle.Popup;
             button7.Location = new Point(23, 212);
             button7.Name = "button7";
             button7.Size = new Size(98, 65);
             button7.TabIndex = 8;
             button7.Text = "7";
-            button7.UseVisualStyleBackColor = true;
+            button7.UseVisualStyleBackColor = false;
             button7.Click += Numbers_Clicked;
             // 
             // button8
             // 
+            button8.BackColor = Color.Thistle;
+            button8.FlatStyle = FlatStyle.Popup;
             button8.Location = new Point(127, 212);
             button8.Name = "button8";
             button8.Size = new Size(101, 65);
             button8.TabIndex = 7;
             button8.Text = "8";
-            button8.UseVisualStyleBackColor = true;
+            button8.UseVisualStyleBackColor = false;
             button8.Click += Numbers_Clicked;
             // 
             // button9
             // 
+            button9.BackColor = Color.Thistle;
+            button9.FlatStyle = FlatStyle.Popup;
             button9.Location = new Point(234, 212);
             button9.Name = "button9";
             button9.Size = new Size(98, 65);
             button9.TabIndex = 6;
             button9.Text = "9";
-            button9.UseVisualStyleBackColor = true;
+            button9.UseVisualStyleBackColor = false;
             button9.Click += Numbers_Clicked;
             // 
             // button1
             // 
+            button1.BackColor = Color.Thistle;
+            button1.FlatStyle = FlatStyle.Popup;
             button1.Location = new Point(23, 354);
             button1.Name = "button1";
             button1.Size = new Size(98, 65);
             button1.TabIndex = 11;
             button1.Text = "1";
-            button1.UseVisualStyleBackColor = true;
+            button1.UseVisualStyleBackColor = false;
             button1.Click += Numbers_Clicked;
             // 
             // button2
             // 
+            button2.BackColor = Color.Thistle;
+            button2.FlatStyle = FlatStyle.Popup;
             button2.Location = new Point(127, 354);
             button2.Name = "button2";
             button2.Size = new Size(101, 65);
             button2.TabIndex = 10;
             button2.Text = "2";
-            button2.UseVisualStyleBackColor = true;
+            button2.UseVisualStyleBackColor = false;
             button2.Click += Numbers_Clicked;
             // 
             // button3
             // 
+            button3.BackColor = Color.Thistle;
+            button3.FlatStyle = FlatStyle.Popup;
             button3.Location = new Point(234, 354);
             button3.Name = "button3";
             button3.Size = new Size(98, 65);
             button3.TabIndex = 9;
             button3.Text = "3";
-            button3.UseVisualStyleBackColor = true;
+            button3.UseVisualStyleBackColor = false;
             button3.Click += Numbers_Clicked;
             // 
             // button4
             // 
+            button4.BackColor = Color.Thistle;
+            button4.FlatStyle = FlatStyle.Popup;
             button4.Location = new Point(23, 283);
             button4.Name = "button4";
             button4.Size = new Size(98, 65);
             button4.TabIndex = 14;
             button4.Text = "4";
-            button4.UseVisualStyleBackColor = true;
+            button4.UseVisualStyleBackColor = false;
             button4.Click += Numbers_Clicked;
             // 
             // button5
             // 
+            button5.BackColor = Color.Thistle;
+            button5.FlatStyle = FlatStyle.Popup;
             button5.Location = new Point(127, 283);
             button5.Name = "button5";
             button5.Size = new Size(101, 65);
             button5.TabIndex = 13;
             button5.Text = "5";
-            button5.UseVisualStyleBackColor = true;
+            button5.UseVisualStyleBackColor = false;
             button5.Click += Numbers_Clicked;
             // 
             // button6
             // 
+            button6.BackColor = Color.Thistle;
+            button6.FlatStyle = FlatStyle.Popup;
             button6.Location = new Point(234, 283);
             button6.Name = "button6";
             button6.Size = new Size(98, 65);
             button6.TabIndex = 12;
             button6.Text = "6";
-            button6.UseVisualStyleBackColor = true;
+            button6.UseVisualStyleBackColor = false;
             button6.Click += Numbers_Clicked;
             // 
             // buttonMinus
             // 
+            buttonMinus.BackColor = Color.Thistle;
+            buttonMinus.FlatStyle = FlatStyle.Popup;
             buttonMinus.Location = new Point(339, 283);
             buttonMinus.Name = "buttonMinus";
             buttonMinus.Size = new Size(98, 65);
             buttonMinus.TabIndex = 17;
             buttonMinus.Text = "-";
-            buttonMinus.UseVisualStyleBackColor = true;
+            buttonMinus.UseVisualStyleBackColor = false;
             buttonMinus.Click += Operation_Clicked;
             // 
             // buttonMultiply
             // 
+            buttonMultiply.BackColor = Color.Thistle;
+            buttonMultiply.FlatStyle = FlatStyle.Popup;
             buttonMultiply.Location = new Point(338, 212);
             buttonMultiply.Name = "buttonMultiply";
             buttonMultiply.Size = new Size(98, 65);
             buttonMultiply.TabIndex = 16;
             buttonMultiply.Text = "x";
-            buttonMultiply.UseVisualStyleBackColor = true;
+            buttonMultiply.UseVisualStyleBackColor = false;
             buttonMultiply.Click += Operation_Clicked;
             // 
             // buttonPlus
             // 
+            buttonPlus.BackColor = Color.Thistle;
+            buttonPlus.FlatStyle = FlatStyle.Popup;
             buttonPlus.Location = new Point(339, 354);
             buttonPlus.Name = "buttonPlus";
             buttonPlus.Size = new Size(98, 65);
             buttonPlus.TabIndex = 15;
             buttonPlus.Text = "+";
-            buttonPlus.UseVisualStyleBackColor = true;
+            buttonPlus.UseVisualStyleBackColor = false;
             buttonPlus.Click += Operation_Clicked;
             // 
             // textBox1
@@ -182,78 +206,94 @@
             // 
             // buttonResult
             // 
+            buttonResult.BackColor = Color.Thistle;
+            buttonResult.FlatStyle = FlatStyle.Popup;
             buttonResult.Location = new Point(338, 428);
             buttonResult.Name = "buttonResult";
             buttonResult.Size = new Size(98, 65);
             buttonResult.TabIndex = 21;
             buttonResult.Text = "=";
-            buttonResult.UseVisualStyleBackColor = true;
+            buttonResult.UseVisualStyleBackColor = false;
             buttonResult.Click += OnButtonResultClicked;
             // 
             // button14
             // 
+            button14.BackColor = Color.Thistle;
+            button14.FlatStyle = FlatStyle.Popup;
             button14.Location = new Point(23, 428);
             button14.Name = "button14";
             button14.Size = new Size(205, 65);
             button14.TabIndex = 20;
             button14.Text = "0";
-            button14.UseVisualStyleBackColor = true;
+            button14.UseVisualStyleBackColor = false;
             button14.Click += Numbers_Clicked;
             // 
             // buttonDot
             // 
+            buttonDot.BackColor = Color.Thistle;
+            buttonDot.FlatStyle = FlatStyle.Popup;
             buttonDot.Location = new Point(234, 428);
             buttonDot.Name = "buttonDot";
             buttonDot.Size = new Size(98, 65);
             buttonDot.TabIndex = 19;
             buttonDot.Text = ".";
-            buttonDot.UseVisualStyleBackColor = true;
+            buttonDot.UseVisualStyleBackColor = false;
             buttonDot.Click += OnSeparatorClicked;
             // 
             // buttonDivide
             // 
+            buttonDivide.BackColor = Color.Thistle;
+            buttonDivide.FlatStyle = FlatStyle.Popup;
             buttonDivide.Location = new Point(338, 141);
             buttonDivide.Name = "buttonDivide";
             buttonDivide.Size = new Size(98, 65);
             buttonDivide.TabIndex = 22;
             buttonDivide.Text = "÷";
-            buttonDivide.UseVisualStyleBackColor = true;
+            buttonDivide.UseVisualStyleBackColor = false;
             buttonDivide.Click += Operation_Clicked;
             // 
             // buttonClear
             // 
+            buttonClear.BackColor = Color.Thistle;
+            buttonClear.FlatStyle = FlatStyle.Popup;
             buttonClear.Location = new Point(127, 141);
             buttonClear.Name = "buttonClear";
             buttonClear.Size = new Size(98, 65);
             buttonClear.TabIndex = 23;
             buttonClear.Text = "🡄";
-            buttonClear.UseVisualStyleBackColor = true;
+            buttonClear.UseVisualStyleBackColor = false;
             buttonClear.Click += OnClearClicked;
             // 
             // button18
             // 
+            button18.BackColor = Color.Thistle;
+            button18.FlatStyle = FlatStyle.Popup;
             button18.Location = new Point(22, 141);
             button18.Name = "button18";
             button18.Size = new Size(98, 65);
             button18.TabIndex = 24;
             button18.Text = "C";
-            button18.UseVisualStyleBackColor = true;
+            button18.UseVisualStyleBackColor = false;
             button18.Click += OnClearEverythingClicked;
             // 
             // buttonChange
             // 
+            buttonChange.BackColor = Color.Thistle;
+            buttonChange.FlatStyle = FlatStyle.Popup;
+            buttonChange.ForeColor = SystemColors.ActiveCaptionText;
             buttonChange.Location = new Point(234, 141);
             buttonChange.Name = "buttonChange";
             buttonChange.Size = new Size(98, 65);
             buttonChange.TabIndex = 25;
             buttonChange.Text = "±";
-            buttonChange.UseVisualStyleBackColor = true;
+            buttonChange.UseVisualStyleBackColor = false;
             buttonChange.Click += OnPlusMinusClicked;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.GradientInactiveCaption;
             ClientSize = new Size(470, 522);
             Controls.Add(buttonChange);
             Controls.Add(button18);
@@ -279,6 +319,7 @@
             MaximizeBox = false;
             Name = "Form1";
             Text = "Form1";
+            Load += InitTimer;
             ResumeLayout(false);
             PerformLayout();
         }
--- WinFormsApp1/Form1.cs
+++ WinFormsApp1/Form1.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Data;
+using System.Linq.Expressions;
 using System.Reflection.Metadata;
 
 namespace WinFormsApp1
@@ -7,8 +8,10 @@ namespace WinFormsApp1
 
     public partial class Form1 : Form
     {
+
+
         private string LastValue = string.Empty;
-        private bool OperationClicked = false;
+        private List<string> Values = new();
         IFormatProvider CultureUS = new System.Globalization.CultureInfo("en-US");
 
         private const string divisionErrorMessage = "Nie mo¿na dzieliæ przez 0";
@@ -18,7 +21,7 @@ namespace WinFormsApp1
             InitializeComponent();
         }
 
-        private void DisableButtons(object sender, EventArgs e)
+        private void DisableButtons()
         {
             buttonChange.Enabled = false;
             buttonDivide.Enabled = false;
@@ -30,14 +33,36 @@ namespace WinFormsApp1
             buttonClear.Enabled = false;
         }
 
+        private void EnableButtons()
+        {
+            buttonChange.Enabled = true;
+            buttonDivide.Enabled = true;
+            buttonMultiply.Enabled = true;
+            buttonMinus.Enabled = true;
+            buttonPlus.Enabled = true;
+            buttonDot.Enabled = true;
+            buttonResult.Enabled = true;
+            buttonClear.Enabled = true;
+        }
+
 
         private void Numbers_Clicked(object sender, EventArgs e)
         {
+            EnableButtons();
+            if (textBox1.Text == divisionErrorMessage)
+            {
+                textBox1.Text = String.Empty;
+            }
             string num = (sender as Button).Text;
-            if (textBox1.Text.Equals(divisionErrorMessage))
+            if (textBox1.Text.Equals("0"))
             {
                 textBox1.Text = num;
                 return;
+            }
+            if (LastValue.Equals("0"))
+            {
+                textBox1.Text = textBox1.Text.Remove(textBox1.Text.Length - 1);
+                LastValue = string.Empty;
             }
             LastValue += num;
             textBox1.Text += num;
@@ -45,10 +70,22 @@ namespace WinFormsApp1
 
         private void Operation_Clicked(object sender, EventArgs e)
         {
-            OperationClicked = true;
-            LastValue = string.Empty;
             var operation = (sender as Button).Text;
-            textBox1.Text += operation;
+            if (!IsPreviousAnOperation())
+            {
+                if (textBox1.Text == "0." || textBox1.Text == String.Empty)
+                {
+                    textBox1.Text = "0";
+                }
+                Values.Add(LastValue);
+                LastValue = string.Empty;
+                textBox1.Text += operation;
+            }
+            else
+            {
+                textBox1.Text = textBox1.Text.Remove(textBox1.Text.Length - 1);
+                textBox1.Text += operation;
+            }
         }
 
 
@@ -63,31 +100,74 @@ namespace WinFormsApp1
                 if (result.Equals("NaN") || double.IsInfinity(resultAsDouble))
                 {
                     textBox1.Text = divisionErrorMessage;
-                    DisableButtons(sender, e);
+                    DisableButtons();
+                    LastValue = string.Empty;
                     return;
                 }
 
-                textBox1.Text = result;
+                textBox1.Text = resultAsDouble.ToString(CultureUS);
 
             }
             catch (DivideByZeroException)
             {
                 textBox1.Text = divisionErrorMessage;
-                DisableButtons(sender, e);   
+                DisableButtons();
+                LastValue = string.Empty;
             }
+            catch (SyntaxErrorException)
+            {
+                MessageBox.Show("Nie mo¿na przeprowadziæ tej operacji");
+                textBox1.Text = string.Empty;
+            }
+
             LastValue = textBox1.Text;
         }
 
         private void OnClearClicked(object sender, EventArgs e)
         {
-            textBox1.Text = textBox1.Text.Remove(textBox1.Text.Length - 1);
-            LastValue = LastValue.Remove(LastValue.Length - 1);
+            if (textBox1.Text.Length > 0)
+            {
+                if (IsPreviousAnOperation())
+                {
+                    if (textBox1.Text[^1] == '-' && (textBox1.Text[^2] == 'x' || textBox1.Text[^2] == '÷' || textBox1.Text[^2] == '+' || textBox1.Text[^2] == '-'))
+                    {
+                        textBox1.Text = textBox1.Text.Remove(textBox1.Text.Length - 1);
+                        return;
+
+                    }
+                    textBox1.Text = textBox1.Text.Remove(textBox1.Text.Length - 1);
+                    if (!textBox1.Text.Contains('x') && !textBox1.Text.Contains('÷') && !textBox1.Text.Contains('+') && !textBox1.Text.Contains("-"))
+                    {
+                        LastValue = textBox1.Text;
+                    }
+                    else
+                    {
+                        LastValue = Values[^1];
+                        Values.RemoveAt(Values.Count - 1);
+                    }
+                    return;
+                }
+                textBox1.Text = textBox1.Text.Remove(textBox1.Text.Length - 1);
+                if (LastValue != string.Empty)
+                {
+                    LastValue = LastValue.Remove(LastValue.Length - 1);
+                }
+            }
+            else
+            {
+                Values.Clear();
+            }
         }
 
         private void OnSeparatorClicked(object sender, EventArgs e)
         {
             if (!LastValue.Contains("."))
             {
+                if (LastValue == string.Empty)
+                {
+                    textBox1.Text += "0";
+                    LastValue += "0";
+                }
                 textBox1.Text += ".";
                 LastValue += ".";
             }
@@ -95,17 +175,53 @@ namespace WinFormsApp1
 
         private void OnClearEverythingClicked(object sender, EventArgs e)
         {
+            EnableButtons();
             textBox1.Text = string.Empty;
+            LastValue = string.Empty;
+            Values.Clear();
         }
 
         private void OnPlusMinusClicked(object sender, EventArgs e)
         {
-            textBox1.Text = textBox1.Text.Remove(textBox1.Text.Length - LastValue.Length);
-            double val = Double.Parse(LastValue) * -1;
-            LastValue = val.ToString();
-            textBox1.Text += LastValue;
+            if (!IsPreviousAnOperation() && LastValue != "0")
+            {
+                textBox1.Text = textBox1.Text.Remove(textBox1.Text.Length - LastValue.Length);
+                double val = double.Parse(LastValue, CultureUS) * -1;
+                LastValue = val.ToString(CultureUS);
+                textBox1.Text += LastValue;
+            }
+        }
+
+        private bool IsPreviousAnOperation()
+        {
+            if (textBox1.Text != string.Empty)
+            {
+                char LastChar = textBox1.Text[^1];
+                if (LastChar == 'x' || LastChar == '÷' || LastChar == '+' || LastChar == '-')
+                {
+                    return true;
+                }
+            }
+
+            return false;
         }
 
 
+
+        #region TEST
+        private System.Windows.Forms.Timer timer1;
+
+        private void InitTimer(object sender, EventArgs e)
+        {
+            timer1 = new System.Windows.Forms.Timer();
+            timer1.Tick += new EventHandler(timer1_Tick);
+            timer1.Interval = 100; // in miliseconds
+            timer1.Start();
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Console.WriteLine($"{LastValue.ToString()}: [{string.Join(", ", Values)}]");
+        }
+        #endregion
     }
 }
